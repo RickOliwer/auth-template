d7f4167331fdce904938325e1b6d1eb54b233484
R C <rickoliwer@gmail.com>
2025-09-19 15:43:38 +0200

    move link

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthClient from "./auth-client";
+
+export default function AuthPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <AuthClient />
+    </div>
+  );
+}
